2787ca4332520959dfe6d67e27370f63dc883e7d
unicsys <unicornsystem4@gmail.com>
2025-09-23 18:27:24 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -33,16 +33,3 @@ export default App;
 
 
 ----------------
-
-  Dear [HR Representative’s Name],
-
-I hope you are doing well. I am in the process of filing my taxes before the October 15 deadline, and I urgently need your assistance with an issue related to my W2.
-
-The W2 I received from RTX has two pages and lists two states—Connecticut (CT) and California (CA). However, I did not work in Connecticut at any time. This is causing complications with my filing, as it looks like the same W2 is showing entries for both states.
-
-I encountered the same problem back in April when I first attempted to file, and I am concerned it has not been corrected. Since I already paid some tax when applying for the extension, it would really help if this could be resolved as soon as possible so I can complete my filing accurately and on time.
-
-Thank you very much for your time and urgent support on this matter.
-
-Best regards,
-Shatad Purohit
