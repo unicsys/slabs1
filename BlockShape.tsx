75c0e1426d032261a5d21b16d89db397245e5845
unicsys <unicornsystem4@gmail.com>
2025-10-14 13:53:13 -0700

    Update BlockShape.tsx

--- BlockShape.tsx
+++ BlockShape.tsx
@@ -1,18 +1,10 @@
-Characteristic Name,Characteristic Type,Feature Name,Feature Type,Nominal Value,Upper Tolerance,Lower Tolerance,Datum Reference
-FLATNESS1,Flatness,PLANE2,Plane,,0.05,-0.05,
-PERPENDICULARITY1,Perpendicularity,PLANE5,Plane,,0.13,-0.13,A
-DIAMETER1,Diameter,SIMPLE_HOLE12,Cylinder,3.3,0.2,-0.2,
-POSITION3,Position,SIMPLE_HOLE12,Cylinder,3.3,0.25,-0.25,A-B
-DIAMETER7,Diameter,CYLINDER-208,Cylinder,6.6,0.254,-0.254,
-POSITION6,Position,CYLINDER-208,Cylinder,6.6,0.5,-0.5,A-B-C(M)
-DIAMETER5,Diameter,HOLE_PATTERN7_3,Cylinder,3.3,0.254,-0.254,
-POSITION5,Position,HOLE_PATTERN7_3,Cylinder,3.3,0.5,-0.5,A-B-C(M)
-DIAMETER2,Diameter,SIMPLE_HOLE16,Cylinder,12.5,0.3,-0.3,
-DIAMETER3,Diameter,SIMPLE_HOLE21,Cylinder,7.5,0.3,-0.3,
-LENGTH2,Length,SLOT5,OppositeParallelLines,20,0.508,-0.508,
-WIDTH6,Width,SLOT5,OppositeParallelLines,5,0.254,-0.254,
-DISTANCEBETWEEN2,DistanceBetween,,,144.4005707,0.5,-0.5,
-DISTANCEBETWEEN4,DistanceBetween,,,73,0.5,-0.5,
-WIDTH2,DistanceBetween,,,93.60057072,0.5,-0.5,
 
-
+✓What Gets Captured
+Concept Definition
+What this data element represents in business terms
+All Column Mappings
+Every database column that represents this concept
+Relationships to Other Concepts
+How this concept connects to other data elements
+Validation Context
+Who validated it, when, and business rules
